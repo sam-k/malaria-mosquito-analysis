3855489f89e5ec2a2ecdc45a0f4c7ed038d2720a
Sam K <samkim1729@gmail.com>
2019-01-06 20:49:35 -0500

    Validate descriptive data

--- Code/spat21_data_checks_mosquitoes.R
+++ Code/spat21_data_checks_mosquitoes.R
@@ -33,6 +33,36 @@ load(CLEANED_FP)  # allspecies_data, anopheles_data, qpcr_data
 
 # Perform data checks for anopheles_data.
 write.log("# ------ VALIDATE ANOPH. DESCRIPTIVE DATA ------ #")
+# Check if village names are consistent.
+anoph_village_discrepancies <- anopheles_data %>%
+  select(household.id, village, sample.id.head, sample.id.abdomen) %>%
+  filter((substr(household.id,1,1)!=substr(sample.id.head,1,1)) & (substr(household.id,1,1)!=substr(sample.id.abdomen,1,1)) &
+         (substr(household.id,1,1)!=substr(village,1,1)))
+write.log("All village names match")
+# Check if heads/abdomens are missing.
+anoph_counts <- anopheles_data %>%
+  select(village, sample.id.head, sample.id.abdomen) %>%
+  group_by(village) %>%
+  summarize(H=sum(!is.na(sample.id.head)), A=sum(!is.na(sample.id.abdomen))) %>%
+  as.data.frame()
+rownames(anoph_counts) <- substr(anoph_counts$village, 1, 1)
+anoph_counts$village <- NULL
+write.table(anoph_counts, col.names=NA, file=LOG_FP, append=TRUE, quote=FALSE, sep="\t")
+write.log()
+write.log("S02 A00001 is missing (as noted in comment)",
+          "K05 A00034, M01 A00058, S07 A00008 are commented as missing, but present in data")
+# Check if abdominal statuses are correct.
+anoph_abd_discrepancies <- anopheles_data %>%
+  select()
 
 # Perform data checks for qpcr_data.
 write.log("# ------ VALIDATE QPCR DATA ------ #")
+
+
+#### -------- export validated data ----------------- ####
+
+
+
+
+#### -------- clean up environment ----------------- ####
+rm(list=ls(pattern="^temp_"))
