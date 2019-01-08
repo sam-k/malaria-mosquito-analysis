0da4f9cf68890962e8a0f3a8a19736a137026704
Sam K <samkim1729@gmail.com>
2019-01-07 21:27:33 -0500

    Check for unmerged data

--- Code/spat21_data_merging_mosquitoes.R
+++ Code/spat21_data_merging_mosquitoes.R
@@ -15,24 +15,28 @@ library(magrittr)
 
 #### --------- set up environment ----------------- ####
 wd <- "~/Projects/Malaria collab/Spatial R21 projects/Spat21 cleaning, analysis/"
-CLEANED_FP <- paste0(wd, "Data/Data Sets/cleaned_data.Rdata")
+VALIDATED_FP  <- paste0(wd, "Data/Data Sets/validated_data.Rdata")
+MERGED_CSV_FP <- paste0(wd, "Data/Data Sets/merged_mosquito_data.csv")
+MERGED_RDS_FP <- paste0(wd, "Data/Data Sets/merged_mosquito_data.rds")
+LOG_FP        <- paste0(wd, "Code/spat21_data_merging_mosquitoes.log")
+close(file(LOG_FP, open="w"))  # clear log file
+write.log <- function(...) {
+  for(temp_output in list(...)) {
+    write(temp_output, file=LOG_FP, append=TRUE)
+  }
+  write("", file=LOG_FP, append=TRUE)
+}
 
 
 #### --------- read in mosquito data ----------------- ####
-load(CLEANED_FP)  # allspecies_data, anopheles_data, qpcr_data
+load(VALIDATED_FP)  # allspecies_data, anopheles_data, qpcr_data
 
 
 #### ------------- merge mosquito data sets ---------------- ####
 
-# Extract sample IDs from anopheles_data.
-anopheles_data$sample.id <- gsub("\\s?H\\s?", " ", anopheles_data$sample.id.head)
+write.log("# ------ MERGE MOSQUITO DATA ------ #")
 
 # Group qpcr_data by sample ID.
-qpcr_data$Sample.ID <- gsub("\\s?[AH]\\s?", " ", qpcr_data$Sample.Name)
-qpcr_data$Head.Abd  <- gsub("[^AH]", "", qpcr_data$Sample.Name)
-qpcr_data %<>%
-  mutate(Head.Abd = factor(Head.Abd)) %>%
-  arrange(Sample.ID, Head.Abd)
 qpcr_groupeddata <- as.data.frame(matrix(nrow=nrow(qpcr_data), ncol=17), stringsAsFactors=FALSE)  # overshoot # of rows
 names(qpcr_groupeddata) <- c("sample.id",
                              "H.HbtubCT1","H.HbtubCT2","H.pfr364CT1","H.pfr364CT2","H.pfr364Q1","H.pfr364Q2","H.Has.Hb","H.Has.Pf",
@@ -50,9 +54,24 @@ for(i in 1:nrow(qpcr_data)) {
   }
 }
 qpcr_groupeddata %<>% filter(!is.na(sample.id))  # trim empty rows
+write.log("Converted qPCR data to wide format by sample ID")
 
 # Merge anopheles_data with qpcr_data.
 merged_data <- left_join(anopheles_data, qpcr_groupeddata, by="sample.id")
+write.log("Merged anopheles descriptive data with wide qPCR data")
+
+
+#### ------------- validate merged data ---------------- ####
+
+# Check if any entries were not merged.
+unmerged_anoph_data <- anopheles_data[-which(anopheles_data$sample.id %in% merged_data$sample.id), ]
+write.log("All descriptive entries were present in qPCR data and merged correctly")
+unmerged_qpcr_data  <- qpcr_groupeddata[-which(qpcr_groupeddata$sample.id %in% merged_data$sample.id), ]
+write.log("qPCR entries K04 00004, K14 00027, M16 00035, S02 00033, S09 00003 were absent from descriptive data and did not merge")
+
+#### -------- export merged data ----------------- ####
+write.csv(merged_data, file=MERGED_CSV_FP, row.names=FALSE)
+saveRDS(merged_data, file=MERGED_RDS_FP)
 
 
 #### -------- clean up environment ----------------- ####
